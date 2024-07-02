90fcb1a4e536a1d66533c8dd3dae419ccade58ae
yashrajmote <yashrajmote2001@gmail.com>
2024-07-02 09:23:36 -0400

    Initial UI

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path'); 
 const app = express();
 const port = 3000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(express.static('views'));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 const apiRoutes = require('./routes/routes');
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/submit', (req, res) => {
+    const formData = req.body;
 
+    //CALCULATIONS
+
+    let result = 0;
+    Object.values(formData).forEach(value => {
+        if (!isNaN(value)) {
+            
+        }
+    })
+
+res.json({ result: result}); // sending result back to client
+
+});
 
 module.exports = router;
