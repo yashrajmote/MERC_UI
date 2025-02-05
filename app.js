09840cd63012e750492c832b83de84046fcaf42c
ShalviMarudkar <101784862+ShalviMarudkar@users.noreply.github.com>
2025-02-06 00:45:30 +0530

    coal table added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path'); 
+const path = require('path');
+const xlsx = require('xlsx'); 
 const app = express();
 const port = 3000;
 
@@ -10,7 +11,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('js'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'slagging.html'));
+  res.sendFile(path.join(__dirname, 'public', 'coal_table.html'));
+});
+
+app.get('/api/data', (req, res) => {
+  const filePath = path.join(__dirname, 'public','slagging_data.xlsx'); // Path to your Excel file
+  const workbook = xlsx.readFile(filePath);
+  const sheetName = workbook.SheetNames[0]; // Assuming the first sheet
+  const sheet = workbook.Sheets[sheetName];
+  const jsonData = xlsx.utils.sheet_to_json(sheet, { header: 1 });
+
+  res.json(jsonData);
 });
 
 
