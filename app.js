cb6f17e2896c946a9457eff9ba491b5f9ab3b4e8
ShalviMarudkar <101784862+ShalviMarudkar@users.noreply.github.com>
2025-02-07 18:22:55 +0530

    Slagging page
    new UI

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('js'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'coal_table.html'));
+  res.sendFile(path.join(__dirname, 'public', 'coal_page.html'));
 });
 
 app.get('/api/data', (req, res) => {
@@ -23,6 +23,37 @@ app.get('/api/data', (req, res) => {
 
   res.json(jsonData);
 });
+
+app.get('/get_coal_types', (req, res) => {
+  const filePath = path.join(__dirname, 'public','slagging_data.xlsx');
+  const workbook = xlsx.readFile(filePath);
+  const sheetName = workbook.SheetNames[0];
+  const sheet = workbook.Sheets[sheetName];
+  const data = xlsx.utils.sheet_to_json(sheet, { header: 1 });
+
+  // Extract column headers to find indices of required properties
+  const headers = data[0];  // The first row contains headers
+  const requiredProperties = ["SiO2", "Al2O3", "Fe2O3", "CaO", "MgO", "Na2O", "K2O", "TiO2", "SO3", "P2O5","Sulphur (S)","MN3O4","Initial Deformation Temp.","Softening/ Spherical Temp.","Hemispherical Temp.","Fluid Temp."];
+
+  // Find indices of required properties
+  const propertyIndices = requiredProperties.map(prop => headers.indexOf(prop));
+
+  // Fetch coal types and their respective properties
+  const coalData = data.slice(1).map(row => {
+      let coalType = row[0]; // First column is coal type
+      let properties = {};
+
+      // Extract required properties
+      requiredProperties.forEach((prop, index) => {
+          properties[prop] = row[propertyIndices[index]];
+      });
+
+      return { coalType, properties };
+  });
+
+  res.json({ coal_data: coalData });
+});
+
 
 
 // Start Server
